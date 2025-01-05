ae00d1c1114566029dd86fbc20214dfc420871df
Marijana Petrovic <slavisapokimica@yahoo.com>
2025-01-05 20:15:23 +0100

    Initial commit for react bff service

---
+++ ReactBFF/Controllers/PizzasController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactBFF.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzasController : ControllerBase
+{
+    private readonly ILogger<PizzasController> _logger;
+
+    public PizzasController(ILogger<PizzasController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Pizza> Get()
+    {
+        List<Pizza> result = [
+            new Pizza
+            {
+                Id = 1,
+                Name = "Focaccia",
+                Ingredients = "Bread with italian olive oil and rosemary",
+                PhotoName = "pizzas/focaccia.jpg",
+                Price = 6,
+                SoldOut = false
+            },
+            new Pizza
+            {
+                Id = 1,
+                Name = "Pizza Margherita",
+                Ingredients = "Tomato and mozarella",
+                PhotoName = "pizzas/margherita.jpg",
+                Price = 10,
+                SoldOut = false
+            }];
+
+        return result;
+    }
+}
---
+++ ReactBFF/Pizza.cs
@@ -0,0 +1,11 @@
+namespace ReactBFF;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+    public float Price { get; set; }
+    public string PhotoName { get; set; }
+    public bool SoldOut {  get; set; }
+}
