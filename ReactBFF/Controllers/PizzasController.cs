3f526c1751c0e418296c679b225a79a661e5088f
Slavisa Pokimica <slavisapokimica@yahoo.com>
2025-01-07 21:40:00 +0100

    config

--- ReactBFF/Controllers/PizzasController.cs
+++ ReactBFF/Controllers/PizzasController.cs
@@ -9,10 +9,13 @@ namespace ReactBFF.Controllers;
 public class PizzasController : ControllerBase
 {
     private readonly ILogger<PizzasController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public PizzasController(ILogger<PizzasController> logger)
+    public PizzasController(ILogger<PizzasController> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [HttpGet]
@@ -25,5 +28,11 @@ public class PizzasController : ControllerBase
         _logger.LogInformation("Returning pizzasss: " + result.Count);
 
         return result;
+    }
+
+    [HttpGet("config")]
+    public string GetConfig()
+    {
+        return _configuration["CosmosDb:ConnectionString"];
     }
 }
